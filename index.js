0e581f900d1f428541a971103f3ac22ccff2e1da
Wolfo-Gaming <37922940+Wolfo-Gaming@users.noreply.github.com>
2022-06-16 08:44:41 +0200

    Fix up kvm support & add uploading

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ app.get('/streams/v1/images.json', async (req, res) => {
       const image_path_releases = __dirname + `\\storage\\${images[i].os}\\${images[i].release}\\${images[i].architecture}\\${images[i].variant}`
       let versions = {}
       fs.readdirSync(image_path_releases).forEach(version_dir => {
-         let files =  fs.readdirSync(image_path_releases + "\\" + version_dir)
+         let files = fs.readdirSync(image_path_releases + "\\" + version_dir.replace(':', '-'))
          let files_list = {};
          files.forEach(file => {
             let type;
@@ -49,22 +49,23 @@ app.get('/streams/v1/images.json', async (req, res) => {
             if (file.includes("squashfs")) type = "squashfs";
             if (file.includes(".vcdiff")) type = "squashfs.vcdiff";
             if (!type) type = file;
-            console.log(image_path_releases + "\\" + version_dir + "\\" + file)
+            console.log(image_path_releases + "\\" + version_dir.replace(':', '-') + "\\" + file)
             files_list[file] = {
                "ftype": type,
                "sha256": hash.createHash(image_path_releases + "\\" + version_dir + "\\" + file),
                "size": fs.statSync(image_path_releases + "\\" + version_dir + "\\" + file).size,
-               "path": `storage/${images[i].os}/${images[i].release}/${images[i].architecture}/${images[i].variant}`+ "/" + version_dir + "/" + file
+               "path": `storage/${images[i].os}/${images[i].release}/${images[i].architecture}/${images[i].variant}` + "/" + version_dir + "/" + file
             }
             if (type == "lxd.tar.xz") {
-               files_list[file].combined_sha256 = hash.createCombinedHash(image_path_releases + "\\" + version_dir + "\\" + file, image_path_releases + "\\" + version_dir + "\\root.tar.xz")
-               files_list[file].combined_rootxz_sha256 = hash.createCombinedHash(image_path_releases + "\\" + version_dir + "\\" + file, image_path_releases + "\\" + version_dir + "\\root.tar.xz")
+               console.log()
+               files_list[file].combined_sha256 = hash.createCombinedHash(image_path_releases + "\\" + version_dir.replace(':', '-') + "\\" + file, image_path_releases + "\\" + version_dir.replace(':', '-') + "\\root.tar.xz")
+               files_list[file].combined_rootxz_sha256 = hash.createCombinedHash(image_path_releases + "\\" + version_dir.replace(':', '-') + "\\" + file, image_path_releases + "\\" + version_dir.replace(':', '-') + "\\root.tar.xz")
                if (files.find(f => f.includes(".qcow2"))) {
-                  files_list[file]["combined_disk-kvm-img_sha256"] = "thegeniuswillbeheresoon"
+                  files_list[file]["combined_disk-kvm-img_sha256"] = hash.createCombinedHash(image_path_releases + "\\" + version_dir.replace(':', '-') + "\\" + file, image_path_releases + "\\" + version_dir.replace(':', '-') + "\\disk.qcow2")
                }
             }
          })
-         versions[version_dir] = {
+         versions[version_dir.replace('-', ':')] = {
             items: files_list
          }
       })
@@ -82,36 +83,70 @@ app.get('/streams/v1/images.json', async (req, res) => {
    return res.json(data)
 })
 var path = require('path');
-app.get("/storage/*", (req,res) => {
+app.get("/storage/*", (req, res) => {
    if (req.url.includes("..")) return res.status(418).send("Nice try bitch.")
    console.log(path.normalize(req.url.toString().replace("/", "\\")))
-   if (fs.statSync("." + req.url).isFile()) {
+   if (fs.statSync("." + req.url.replace(':', '-')).isFile()) {
       if (req.path.includes('tar.xz')) {
          res.setHeader("Accept-Ranges", "Bytes")
-         res.setHeader("Content-Length", fs.statSync("." + path.normalize(req.url.toString().replace("/", "\\"))).size)
+         res.setHeader("Content-Length", fs.statSync("." + path.normalize(req.url.toString().replace("/", "\\").replace(':', '-'))).size)
          res.setHeader("Content-Type", "application/x-xz")
+      } else {
+         res.setHeader("Accept-Ranges", "Bytes")
+         res.setHeader("Content-Length", fs.statSync("." + path.normalize(req.url.toString().replace("/", "\\").replace(':', '-'))).size)
       }
-      fs.createReadStream("." + path.normalize(req.url.toString().replace("/", "\\"))).pipe(res)
+      fs.createReadStream("." + path.normalize(req.url.toString().replace("/", "\\").replace(':', '-'))).pipe(res)
    } else {
       return res.status(418).send("Nice try bitch.")
    }
 
 })
 app.post('/images', function (req, res) {
-   const d = new Date()
-     if (!req.files["rootfs"]) {
-         if (!req.files['kvmdisk']) return res.json({error:"KVM image not present in form body"})
+   try {
+      const d = new Date()
+      if (!req.files["rootfs"]) {
+         if (!req.files['kvmdisk']) return res.json({ error: "KVM image not present in form body" })
          var { aliases, os, release, releasetitle, variant, architecture, requirements } = req.body
          fs.mkdirSync('./path/to/my/directory', { recursive: true })
-     } else {
-      if (!req.files['rootfs']) return res.json({error:"Rootfs not present in form body"})
-      if (!req.files['lxdmeta']) return res.json({error:"LXD meta not present in form body"})
-      var zero = d.getMonth() < 10 ? "0":""
-      var zeroday = d.getDate() < 10 ? "0":""
-      var date = `${d.getFullYear()}${zero + d.getMonth()}${zeroday + d.getDate()}_${d.getHours() + ":" + d.getMinutes()}`
-      var { aliases, os, release, releasetitle, variant, architecture, requirements } = req.body
-      fs.mkdirSync(path.normalize(`./storage/${os}/${release}/${architecture}/${variant}/`), { recursive: true })
-     }
+      } else {
+         if (!req.files['rootfs']) return res.json({ error: "Rootfs not present in form body" })
+         if (!req.files['lxdmeta']) return res.json({ error: "LXD meta not present in form body" })
+         var zero = d.getMonth() < 10 ? "0" : ""
+         var zeroday = d.getDate() < 10 ? "0" : ""
+         var zerohours = d.getHours() < 10 ? "0" : ""
+         var zerominutes = d.getMinutes() < 10 ? "0" : ""
+         var date = `${d.getFullYear()}${zero + d.getMonth()}${zeroday + d.getDate()}_${zerohours + d.getHours() + "-" + zerominutes + d.getMinutes()}`
+         var { aliases, os, release, releasetitle, variant, architecture, requirements } = req.body
+         if (!aliases || !os || !release || !releasetitle || !variant || !architecture) return res.json({ error: "Missing required fields" })
+         fs.mkdirSync(path.normalize(`./storage/${os}/${release}/${architecture}/${variant}/${date}`), { recursive: true })
+         fs.writeFileSync(path.normalize(`./storage/${os}/${release}/${architecture}/${variant}/${date}/root.tar.xz`), req.files['rootfs'].data)
+         fs.writeFileSync(path.normalize(`./storage/${os}/${release}/${architecture}/${variant}/${date}/lxd.tar.xz`), req.files['lxdmeta'].data)
+         prisma.image.create({
+            data: {
+               os: os,
+               release: release,
+               architecture: architecture,
+               variant: variant,
+               aliases: aliases,
+               release_title: releasetitle,
+               lxd_requirements: requirements ? JSON.stringify(requirements) : '{ }',
+            }
+         }).then(image => {
+            res.json({
+               success: true,
+               data: image
+            })
+         })
+
+      }
+   } catch (error) {
+      console.log(error)
+      res.status(500).send({
+         success: false,
+         error: error
+      })
+   }
+
 });
 
 app.listen(3002)
