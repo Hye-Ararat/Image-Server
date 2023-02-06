9a71b1b2818d9ef43a393340c49beff416566180
Joseph Maldjian <josephavm@icloud.com>
2023-02-05 18:46:00 -0600

    Bugfixes and performance improvements

--- index.js
+++ index.js
@@ -7,6 +7,7 @@
    const { PrismaClient } = require("@prisma/client");
    const fs = require('fs');
    const hash = require('./lib/hash')
+   const cors = require('cors')
 
    const prisma = new PrismaClient()
    function cacheRefresh() {
@@ -27,6 +28,7 @@
       tempFileDir: "./tmp"
    }));
    app.use(express.urlencoded({ extended: true }));
+   app.use(cors())
    app.get("/streams/v1/index.json", async (req, res) => {
       const images = await prisma.image.findMany({});
       let products = [];
@@ -51,13 +53,13 @@
       var data = JSON.parse(fs.readFileSync('./cache.json', 'utf8'))
       return res.json(data)
    })
-   app.get("/findImageId", (req, res) => {
+   app.get("/findImageId", async (req, res) => {
       let { os, release, architecture, variant } = req.query
       os = os.toLowerCase()
       release = release.toLowerCase()
       architecture = architecture.toLowerCase()
       variant = variant.toLowerCase()
-      const image = prisma.image.findFirst({
+      const image = await prisma.image.findFirst({
          where: {
             os: os,
             release: release,
@@ -70,20 +72,154 @@
             id: image.id
          })
       }
-      return res.json({
+      return res.status(404).send({
          error: "Image not found"
       })
    })
-   app.get("/image/:id/extension/:extension", async (req, res) => {
-      let extension = sanitize(req.params.extension)
+   app.get("/image/:id/extensions", async (req, res) => {
       let id = sanitize(req.params.id)
       const image = await prisma.image.findFirst({
          where: {
             id: id
          }
       })
+      if (!image) return res.json({ error: "Image not found" });
+      let extensions = []
+      let data;
+      try {
+         data = fs.readdirSync(`./storage/${image.os.toLowerCase()}/${image.release.toLowerCase()}/extensions`)
+      } catch (error) {
+         return res.json({
+            extensions: []
+         })
+      }
+      for (let i = 0; i < data.length; i++) {
+         if (data[i].endsWith(".json")) {
+
+         } else {
+            extensions.push({
+               name: (data[i].replace(".araratExtension.js", "")).replace(".lavaExtension.js", ""),
+               type: data[i].split(".")[1].split("Extension")[0]
+            })
+         }
+      }
+      return res.json({
+         extensions: extensions
+      })
+   })
+   app.get("/image/:id/widgets", async (req, res) => {
+      let id = sanitize(req.params.id)
+      const image = await prisma.image.findFirst({
+         where: {
+            id: id
+         }
+      })
+      if (!image) return res.json({ error: "Image not found" });
+      let widgets = []
+      let data;
+      try {
+         data = fs.readdirSync(`./storage/${image.os.toLowerCase()}/${image.release.toLowerCase()}/widgets`)
+      } catch (error) {
+         return res.json({
+            widgets: []
+         })
+      }
+      for (let i = 0; i < data.length; i++) {
+         widgets.push({
+            name: (data[i].replace(".araratWidget.js", "")).replace(".lavaWidget.js", ""),
+            type: data[i].split(".")[1].split("Widget")[0]
+         })
+      }
+      return res.json({
+         widgets: widgets
+      })
+   })
+   app.get("/image/:id/widget/:widget", async (req, res) => {
+      let widget = sanitize(req.params.widget)
+      let id = sanitize(req.params.id)
+      console.log(id)
+      const image = await prisma.image.findFirst({
+         where: {
+            id: id
+         }
+      })
       if (image) {
-         let file = fs.readFileSync(`./storage/${image.os.toLowerCase()}/${image.release.toLowerCase()}/extensions/${extension}.extension.js`, 'utf8')
+         let file;
+         try {
+            if (req.query.type == "ararat") {
+               file = fs.readFileSync(`./storage/${image.os.toLowerCase()}/${image.release.toLowerCase()}/widgets/${widget}.araratWidget.js`, 'utf8')
+            } else if (req.query.type == "lava") {
+               file = fs.readFileSync(`./storage/${image.os.toLowerCase()}/${image.release.toLowerCase()}/widgets/${widget}.lavaWidget.js`, 'utf8')
+            } else {
+               file = fs.readFileSync(`./storage/${image.os.toLowerCase()}/${image.release.toLowerCase()}/widgets/${widget}.araratWidget.js`, 'utf8')
+            }
+         } catch {
+            return res.json({
+               error: "Widget not found"
+            })
+         }
+         return res.send(file)
+      } else {
+         return res.json({
+            error: "Image not found"
+         })
+      }
+   })
+   app.get("/image/:id/extension/:extension", async (req, res) => {
+      let extension = sanitize(req.params.extension)
+      let id = sanitize(req.params.id)
+      console.log(id)
+      const image = await prisma.image.findFirst({
+         where: {
+            id: id
+         }
+      })
+      if (image) {
+         let file;
+         try {
+            if (req.query.type == "ararat") {
+               file = fs.readFileSync(`./storage/${image.os.toLowerCase()}/${image.release.toLowerCase()}/extensions/${extension}.araratExtension.js`, 'utf8')
+            } else if (req.query.type == "lava") {
+               file = fs.readFileSync(`./storage/${image.os.toLowerCase()}/${image.release.toLowerCase()}/extensions/${extension}.lavaExtension.js`, 'utf8')
+            } else {
+               file = fs.readFileSync(`./storage/${image.os.toLowerCase()}/${image.release.toLowerCase()}/extensions/${extension}.araratExtension.js`, 'utf8')
+            }
+         } catch {
+            return res.json({
+               error: "Extension not found"
+            })
+         }
+         return res.send(file)
+      } else {
+         return res.json({
+            error: "Image not found"
+         })
+      }
+   })
+   app.get("/image/:id/extensionInfo/:extension", async (req, res) => {
+      let extension = sanitize(req.params.extension)
+      let id = sanitize(req.params.id)
+      console.log(id)
+      const image = await prisma.image.findFirst({
+         where: {
+            id: id
+         }
+      })
+      if (image) {
+         let file;
+         try {
+            if (req.query.type == "ararat") {
+               file = fs.readFileSync(`./storage/${image.os.toLowerCase()}/${image.release.toLowerCase()}/extensions/${extension}.araratExtensionInfo.json`, 'utf8')
+            } else if (req.query.type == "lava") {
+               file = fs.readFileSync(`./storage/${image.os.toLowerCase()}/${image.release.toLowerCase()}/extensions/${extension}.lavaExtensionInfo.json`, 'utf8')
+            } else {
+               file = fs.readFileSync(`./storage/${image.os.toLowerCase()}/${image.release.toLowerCase()}/extensions/${extension}.araratExtensionInfo.json`, 'utf8')
+            }
+         } catch {
+            return res.json({
+               error: "Extension not found"
+            })
+         }
          return res.send(file)
       } else {
          return res.json({
@@ -122,7 +258,7 @@
             var zerohours = d.getHours() < 10 ? "0" : ""
             var zerominutes = d.getMinutes() < 10 ? "0" : ""
             var date = `${d.getFullYear()}${zero + d.getMonth()}${zeroday + d.getDate()}_${zerohours + d.getHours() + "-" + zerominutes + d.getMinutes()}`
-            var { aliases, os, release, releasetitle, variant, architecture, requirements } = req.body
+            var { aliases, os, release, releasetitle, variant, architecture, requirements, properties } = req.body
             if (!aliases || !os || !release || !releasetitle || !variant || !architecture) return res.json({ error: "Missing required fields" })
             let alrExisted = fs.existsSync(path.normalize(`./storage/${os.toLowerCase()}/${release.toLowerCase()}/${architecture}/${variant}`))
             fs.mkdirSync(path.normalize(`./storage/${os.toLowerCase()}/${release.toLowerCase()}/${architecture}/${variant}/${date}`), { recursive: true })
@@ -130,16 +266,24 @@
 
             fs.renameSync(req.files['kvmdisk'].tempFilePath, path.normalize(`./storage/${os.toLowerCase()}/${release.toLowerCase()}/${architecture}/${variant}/${date}/disk.qcow2`))
             fs.renameSync(req.files['lxdmeta'].tempFilePath, path.normalize(`./storage/${os.toLowerCase()}/${release.toLowerCase()}/${architecture}/${variant}/${date}/lxd.tar.xz`))
+            /*Properties example
+            {
+               "environment": [{"key": "key", "value": "value"}],
+               "startup": "echo startup example and env vars work like $EXAMPLE"
+            }
+            */
             if (!alrExisted) {
+               
                prisma.image.create({
                   data: {
-                     os: os,
-                     release: release,
-                     architecture: architecture,
-                     variant: variant,
-                     aliases: aliases,
-                     release_title: releasetitle,
+                     os: os.toLowerCase(),
+                     release: release.toLowerCase(),
+                     architecture: architecture.toLowerCase(),
+                     variant: variant.toLowerCase(),
+                     aliases: aliases.toLowerCase(),
+                     release_title: releasetitle.toLowerCase(),
                      lxd_requirements: requirements ? JSON.stringify(requirements) : '{ }',
+                     properties: properties ? JSON.stringify(properties) : '{ }',
                   }
                }).then(image => {
                   res.json({
@@ -161,7 +305,7 @@
             var zerohours = d.getHours() < 10 ? "0" : ""
             var zerominutes = d.getMinutes() < 10 ? "0" : ""
             var date = `${d.getFullYear()}${zero + d.getMonth()}${zeroday + d.getDate()}_${zerohours + d.getHours() + "-" + zerominutes + d.getMinutes()}`
-            var { aliases, os, release, releasetitle, variant, architecture, requirements } = req.body
+            var { aliases, os, release, releasetitle, variant, architecture, requirements, properties } = req.body
             if (!aliases || !os || !release || !releasetitle || !variant || !architecture) return res.json({ error: "Missing required fields" })
             let alrExisted = fs.existsSync(path.normalize(`./storage/${os.toLowerCase()}/${release.toLowerCase()}/${architecture}/${variant}`))
             fs.mkdirSync(path.normalize(`./storage/${os.toLowerCase()}/${release.toLowerCase()}/${architecture}/${variant}/${date}`), { recursive: true })
@@ -177,6 +321,7 @@
                      aliases: aliases,
                      release_title: releasetitle,
                      lxd_requirements: requirements ? JSON.stringify(requirements) : '{ }',
+                     properties: properties ? JSON.stringify(properties) : '{ }',
                   }
                }).then(image => {
                   res.json({
